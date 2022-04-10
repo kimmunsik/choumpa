4f8607dd0020ae53b081ce69e72f9d87d922b7f8
kimmunsik <103314906+kimmunsik@users.noreply.github.com>
2022-04-10 17:27:13 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -4,7 +4,16 @@ basic.pause(1000)
 basic.forever(function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
     거리 = maqueen.Ultrasonic(PingUnit.Centimeters)
-    if (true) {
-    	
+    if (거리 < 10 && 거리 != 0) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(1000)
+        if (randint(1, 2) == 1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 100)
+        }
+        basic.pause(500)
     }
 })
